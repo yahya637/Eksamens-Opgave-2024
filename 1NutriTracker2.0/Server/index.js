99d0b28e62d3804d8a7b8eacb8d24f6323f3fbf6
Gumxr <algu23ac@student.cbs.dk>
2024-04-20 13:57:15 +0200

    Ændret items til users

--- 1NutriTracker2.0/Server/index.js
+++ 1NutriTracker2.0/Server/index.js
@@ -19,7 +19,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.static(path.join(__dirname, "../Public/StartPage/SignUpPage")));
 app.use(express.static(path.join(__dirname, "../Public/StartPage/HomePage")));
 
-app.use("/items", items); // Route for the right route
+app.use("/users", items); // Route for the right route
 // Start the server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
