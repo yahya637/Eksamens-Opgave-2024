daaff7eaa6d7b232824c6a7e9d534c68f679b3f0
Gumxr <algu23ac@student.cbs.dk>
2024-05-03 11:20:09 +0200

    1/3 af mealtracker virker

--- 1NutriTracker2.0/Server/mealtracker1.js
+++ 1NutriTracker2.0/Server/mealtracker1.js
@@ -26,7 +26,7 @@ router.get('/:userId', async (req, res) => {
 });
 
 // POST new meal
-router.post('/:userId/intake', async (req, res) => {
+router.post('/:userId/register', async (req, res) => {
   const userId = req.params.userId;
   const intakeDetails = req.body;
   console.log('Creating new intake for user', userId, ':', intakeDetails);
@@ -40,6 +40,30 @@ router.post('/:userId/intake', async (req, res) => {
       res.status(500).json({ error: 'Error creating new intake in the database', details: err.message });
   }
 });
+
+
+// GET User meals by user ID, optionally filtered by meal ID
+router.get('/:userId/intake', async (req, res) => {
+  const userId = req.params.userId;
+  const mealId = req.query.mealId; // Obtain mealId from query parameters if it exists
+
+  console.log(`Fetching user meals for user ID: ${userId}`, mealId ? `and meal ID: ${mealId}` : '');
+
+  try {
+      const meals = await database.getIntakeMealsByUserId(userId, mealId);
+      if (meals.length === 0) {
+          console.log('No meals found for this user:', userId);
+          return res.status(404).json({ error: 'No meals found for this user' });
+      }
+      console.log(`Sending back meals data for user ID: ${userId}`, meals);
+      res.status(200).json(meals);
+  }
+  catch (err) {
+      console.error('Error fetching user meals:', err);
+      res.status(500).json({ error: 'Error fetching user meals from the database', details: err.message });
+  }
+});
+
 
 
   
